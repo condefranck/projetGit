d2489fc136c193b6ba1afc11adefed5137fff31d
Franck CONDÉ <innovations.franck@gmail.com>
2018-09-11 20:36:42 +0000

    ma première modif bonjour

---
+++ src/ci/objis/Bonjour.java
@@ -0,0 +1,23 @@
+package ci.objis;
+
+/**
+ * 
+ * @author pc
+ * @version 1.0
+ */
+
+
+public class Bonjour {
+	
+	/**
+	 * 
+	 * @param args - Chaine de caracrtère qui sera passée en argument de la ligne de commande
+	 * @return int - 
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Ecrire bonjour dans la console
+		System.out.println("Bonjour Eclipse");
+	}
+
+}
